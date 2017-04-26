0d588ba7f81daee0805145f40ff4bef0d5b3b8fa
Roberto Lo Giacco <rlogiacco@gmail.com>
2017-04-27 01:31:06 +0200

    added examples

---
+++ examples/Stack/Stack.ino
@@ -0,0 +1,24 @@
+#include <CircularBuffer.h>
+
+CircularBuffer<unsigned int, 100> stack;
+
+#define SAMPLE_PIN A0
+void setup() {
+	Serial.begin(9600);
+	pinMode(SAMPLE_PIN, INPUT);
+}
+
+// stacks up pin state changes printing in reverse order when maxed
+void loop() {
+	unsigned int sample = analogRead(SAMPLE_PIN);
+	if (sample != stack.last()) {
+		stack.push(sample);
+	}
+	if (stack.isFull()) {
+		Serial.println("Stack is full:");
+		while (!stack.isEmpty()) {
+			Serial.print("  #");
+			Serial.println(stack.pop());
+		}
+	}
+}
