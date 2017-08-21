555723b97f669b83516dd7dcc6f0e24005d9c6dd
Roberto Lo Giacco <rlogiacco@gmail.com>
2017-08-21 23:55:19 +0200

    added examples for custom data types

---
+++ examples/Object/Object.ino
@@ -0,0 +1,34 @@
+#include <CircularBuffer.h>
+#include "Record.h"
+
+CircularBuffer<Record*, 10> buffer;
+
+#define SAMPLE_PIN A0
+
+void setup() {
+	Serial.begin(9600);
+	pinMode(SAMPLE_PIN, INPUT);
+	Serial.println("STARTING UP");
+}
+
+void printFn(Print* out, Record* r) {
+	r->print(out);
+}
+
+void loop() {
+	unsigned int sample = analogRead(SAMPLE_PIN);
+	if (sample != buffer.first()->value()) {
+		Record* record = new Record(millis(), sample);
+		buffer.unshift(record);
+		Serial.println("---");
+		delay(50);
+	}
+	if (buffer.isFull()) {
+		Serial.println("Queue is full:");
+		while (!buffer.isEmpty()) {
+			buffer.shift()->print(&Serial);
+			Serial.println();
+		}
+		Serial.println("START AGAIN");
+	}
+}
---
+++ examples/Struct/Struct.ino
@@ -0,0 +1,48 @@
+#include <CircularBuffer.h>
+
+namespace data {
+	typedef struct {
+		unsigned long time;
+		unsigned int value;
+		bool flag;
+	} record;
+
+	void print(record r) {
+		Serial.print(r.time);
+		Serial.print("  ");
+		Serial.print(r.value);
+		Serial.print("  ");
+		Serial.print(r.flag);
+	}
+}
+
+CircularBuffer<data::record, 10> stack;
+
+#define SAMPLE_PIN A0
+
+void setup() {
+	Serial.begin(9600);
+	pinMode(SAMPLE_PIN, INPUT);
+	Serial.println("STARTING UP");
+}
+
+void printFn(Print* out, data::record r) {
+	out->print(r.time);
+}
+
+void loop() {
+	unsigned int sample = analogRead(SAMPLE_PIN);
+	if (sample != stack.last().value) {
+		stack.push(data::record{millis(), sample, sample > 512});
+		Serial.println("---");
+		delay(50);
+	}
+	if (stack.isFull()) {
+		Serial.println("Stack is full:");
+		while (!stack.isEmpty()) {
+			data::print(stack.shift());
+			Serial.println();
+		}
+		Serial.println("START AGAIN");
+	}
+}
