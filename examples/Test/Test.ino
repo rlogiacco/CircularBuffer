e24ff667cb815351e029ecb33be148230a8fea04
Roberto Lo Giacco <rlogiacco@gmail.com>
2017-08-21 20:31:38 +0200

    improved Test.ino implementation

--- examples/Test/Test.ino
+++ examples/Test/Test.ino
@@ -1,123 +1,195 @@
+#define CIRCULAR_BUFFER_DEBUG
 #include <CircularBuffer.h>
 
 CircularBuffer<char, 10> buffer;
 
 void printBuffer() {
-	Serial.print("[");
-	for (unsigned int i = 0; i < buffer.size() - 1; i++) {
-		Serial.print(buffer[i]);
-		Serial.print(",");
-	}
-	Serial.print(buffer[buffer.size() - 1]);
-	Serial.print("] (");
-
-	Serial.print(buffer.size());
-	Serial.print("/");
-	Serial.print(buffer.size() + buffer.available());
 	if (buffer.isEmpty()) {
-		Serial.print(" empty");
-	} else if (buffer.isFull()) {
-		Serial.print(" full");
-	}
+		Serial.println("empty");
+	} else {
+		Serial.print("[");
+		for (unsigned int i = 0; i < buffer.size() - 1; i++) {
+			Serial.print(buffer[i]);
+			Serial.print(",");
+		}
+		Serial.print(buffer[buffer.size() - 1]);
+		Serial.print("] (");
 
-	Serial.println(")");
+		Serial.print(buffer.size());
+		Serial.print("/");
+		Serial.print(buffer.size() + buffer.available());
+		if (buffer.isFull()) {
+			Serial.print(" full");
+		}
+
+		Serial.println(")");
+	}
 }
 
 void setup() {
 	Serial.begin(9600);
-	buffer.push('a');
-	buffer.push('b');
-	buffer.push('c');
-	buffer.push('d');
-	buffer.push('e');
-	buffer.push('f');
-	buffer.push('g');
-	buffer.push('h');
-	buffer.push('j');
-	buffer.push('k');
-	printBuffer(); Serial.println();
+	Serial.println(" ### RESET ### ");
 
-	Serial.println("unshift('X')");
-	Serial.print(buffer.unshift('X'));
-	Serial.print(" leaving buffer as "); 
-	printBuffer(); Serial.println();
-
-	Serial.println("push('#')");
-	Serial.print(buffer.push('#'));
-	Serial.print(" leaving buffer as "); 
-	printBuffer(); Serial.println();
+	buffer.push('A');
+	buffer.push('B');
+	buffer.push('C');
+	buffer.push('D');
+	buffer.push('E');
+	buffer.push('F');
+	buffer.push('G');
+	buffer.push('H');
+	buffer.push('I');
+	buffer.push('J');
+	printBuffer();
+	Serial.println(); delay(250);
 
 	Serial.println("first()");
 	Serial.print(buffer.first());
-	Serial.print(" leaving buffer as "); 
-	printBuffer(); Serial.println();
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
 
 	Serial.println("last()");
 	Serial.print(buffer.last());
-	Serial.print(" leaving buffer as "); 
-	printBuffer(); Serial.println();
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
+
+	Serial.println("unshift('X')");
+	Serial.print(buffer.unshift('X') ? "true" : "false");
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
+
+	Serial.println("push('Z')");
+	Serial.print(buffer.push('Z') ? "true" : "false");
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
 
 	Serial.println("pop()");
 	Serial.print(buffer.pop());
-	Serial.print(" leaving buffer as "); 
-	printBuffer(); Serial.println();
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
 
 	Serial.println("pop()");
 	Serial.print(buffer.pop());
-	Serial.print(" leaving buffer as "); 
-	printBuffer(); Serial.println();
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
+
+	Serial.println("pop()");
+	Serial.print(buffer.pop());
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
+
+	Serial.println("pop()");
+	Serial.print(buffer.pop());
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
+
+	Serial.println("pop()");
+	Serial.print(buffer.pop());
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
 
 	Serial.println("shift()");
 	Serial.print(buffer.shift());
-	Serial.print(" leaving buffer as "); 
-	printBuffer(); Serial.println();
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
 
 	Serial.println("shift()");
 	Serial.print(buffer.shift());
-	Serial.print(" leaving buffer as "); 
-	printBuffer(); Serial.println();
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
+
+	Serial.println("shift()");
+	Serial.print(buffer.shift());
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
+
+	Serial.println("shift()");
+	Serial.print(buffer.shift());
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
+
+	Serial.println("shift()");
+	Serial.print(buffer.shift());
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
+
+	Serial.println("push('Z')");
+	Serial.print(buffer.push('Z') ? "true" : "false");
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
+
+	Serial.println("push('X')");
+	Serial.print(buffer.push('X') ? "true" : "false");
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
 
 	Serial.println("clear()");
 	buffer.clear();
-	Serial.print("buffer is "); 
-	printBuffer(); Serial.println();
+	Serial.print("buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
 
 	Serial.println("unshift('A')");
-	Serial.print(buffer.unshift('A'));
-	Serial.print(" leaving buffer as "); 
-	printBuffer(); Serial.println();
+	Serial.print(buffer.unshift('A') ? "true" : "false");
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
 
 	Serial.println("push('Z')");
-	Serial.print(buffer.push('Z'));
-	Serial.print(" leaving buffer as "); 
-	printBuffer(); Serial.println();
+	Serial.print(buffer.push('Z') ? "true" : "false");
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
 
 	Serial.println("buffer[5]");
 	Serial.print(buffer[5]);
-	Serial.print(" leaving buffer as "); 
-	printBuffer(); Serial.println();
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
 
 	Serial.println("buffer[15]");
 	Serial.print(buffer[15]);
-	Serial.print(" leaving buffer as "); 
-	printBuffer(); Serial.println();
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
 
 	Serial.println("clear()");
 	buffer.clear();
-	Serial.print("buffer is "); 
-	printBuffer(); Serial.println();
+	Serial.print("buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
 
-	Serial.println("pop()");
+	// The following operations will crash the firmware and cause a reset
+
+	Serial.println("CRASH TEST");
+
 	Serial.print(buffer.pop());
-	Serial.print(" leaving buffer as "); 
-	printBuffer(); Serial.println();
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
 
-	Serial.println("shift()");
 	Serial.print(buffer.shift());
-	Serial.print(" leaving buffer as "); 
-	printBuffer(); Serial.println();
+	Serial.print(" buffer is ");
+	printBuffer();
+	Serial.println(); delay(250);
 }
 
 void loop() {
-	
+
 }
