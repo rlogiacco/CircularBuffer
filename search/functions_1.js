d2d3de7e20b55bfa620adf8db567832ad7aa7863
rlogiacco <rlogiacco@users.noreply.github.com>
2024-01-26 22:37:54 +0000

    deploy: 8f9f476681d7c47918847849ee6a6999dc38b585

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['circularbuffer_0',['CircularBuffer',['../class_circular_buffer.html#a4aeba2ac5ecf5348ee4b38c6efbb1243',1,'CircularBuffer']]],
-  ['clear_1',['clear',['../class_circular_buffer.html#acef199d78df0e0d0ee3996ad251ad90c',1,'CircularBuffer']]]
+  ['clear_1',['clear',['../class_circular_buffer.html#acef199d78df0e0d0ee3996ad251ad90c',1,'CircularBuffer']]],
+  ['copytoarray_2',['copyToArray',['../class_circular_buffer.html#a4b21c32bc92aa54560a49c5fc72b2cf1',1,'CircularBuffer::copyToArray(T *out) const'],['../class_circular_buffer.html#ae9b317d2673db8674924d686da31977d',1,'CircularBuffer::copyToArray(R *out, R(&amp;convertFn)(const T &amp;)) const']]]
 ];
